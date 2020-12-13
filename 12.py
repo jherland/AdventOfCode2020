bdd8dc0dc5057a8fb9cc8d1f95e978f18b313573
Johan Herland <johan@herland.net>
2020-12-13 01:08:53 +0100

    day 12: Lose the enums and some typing

--- 12.py
+++ 12.py
@@ -1,40 +1,10 @@
 from dataclasses import dataclass
-from enum import Enum
-from typing import Dict, Union
-
-CardDir = Enum('CardDir', 'N S E W')
-TurnDir = Enum('TurnDir', 'L R')
-MoveDir = Enum('MoveDir', 'F')
-AnyDir = Union[CardDir, TurnDir, MoveDir]
-
-
-@dataclass(frozen=True)
-class Instruction:
-    dir: AnyDir
-    amt: int
-
-    @classmethod
-    def parse(cls, s: str) -> 'Instruction':
-        Dirs: Dict[str, AnyDir] = {
-            'N': CardDir.N,
-            'S': CardDir.S,
-            'E': CardDir.E,
-            'W': CardDir.W,
-            'L': TurnDir.L,
-            'R': TurnDir.R,
-            'F': MoveDir.F,
-        }
-        dir = Dirs[s[0]]
-        amt = int(s[1:])
-        if isinstance(dir, TurnDir):
-            assert amt % 90 == 0
-        assert amt > 0
-        return cls(dir, amt)
 
 
 def parse(f):
     for line in f:
-        yield Instruction.parse(line.rstrip())
+        assert line[0] in 'NSEWLRF'
+        yield line[0], int(line[1:].rstrip())
 
 
 @dataclass
@@ -49,74 +19,40 @@ class Coord:
         return self.__class__(self.y * other, self.x * other)
 
 
-def move(pos: Coord, dir: CardDir, amt: int) -> Coord:
+def move(pos: Coord, dir: str, amt: int) -> Coord:
     '''Move from pos in a cardinal direction by the given amount.'''
     dpos = {
-        CardDir.N: Coord(+1, 0),
-        CardDir.S: Coord(-1, 0),
-        CardDir.E: Coord(0, +1),
-        CardDir.W: Coord(0, -1),
+        'N': Coord(+1, 0),
+        'S': Coord(-1, 0),
+        'E': Coord(0, +1),
+        'W': Coord(0, -1),
     }[dir]
     return pos + (dpos * amt)
 
 
-def turn(dir: CardDir, way: TurnDir, amt: int) -> CardDir:
-    '''Turn left/right from a given start direction by the given amount.'''
-    circle = [
-        (CardDir.N, CardDir.W),
-        (CardDir.W, CardDir.S),
-        (CardDir.S, CardDir.E),
-        (CardDir.E, CardDir.N),
-    ]
-    next_dir = {
-        TurnDir.L: {a: b for a, b in circle},
-        TurnDir.R: {b: a for a, b, in circle},
+def turn(dir: str, way: str, amt: int) -> str:
+    '''Turn left/right from a given direction by the given amount.'''
+    clockwise = [('N', 'E'), ('E', 'S'), ('S', 'W'), ('W', 'N')]
+    turner = {
+        'L': {b: a for a, b in clockwise},
+        'R': {a: b for a, b in clockwise},
     }
     assert amt % 90 == 0 and amt >= 0
-    while amt > 0:
-        dir = next_dir[way][dir]
-        amt -= 90
+    for _ in range(amt // 90):
+        dir = turner[way][dir]
     return dir
 
 
-@dataclass
-class Ship:
-    pos: Coord = Coord(0, 0)  # current position
-    facing: CardDir = CardDir.E  # facing direction
-
-    def move(self, instr: Instruction) -> None:
-        if isinstance(instr.dir, CardDir):
-            self.pos = move(self.pos, instr.dir, instr.amt)
-        elif isinstance(instr.dir, TurnDir):
-            self.facing = turn(self.facing, instr.dir, instr.amt)
-        elif isinstance(instr.dir, MoveDir):
-            self.move(Instruction(self.facing, instr.amt))
-        else:
-            raise NotImplementedError(instr)
-
-    def move_by(self, vector: Coord, scale: int) -> None:
-        self.pos += vector * scale
-
-
-@dataclass
-class Waypoint:
-    pos: Coord  # current position, relative to ship
-
-    def move(self, instr: Instruction) -> None:
-        if isinstance(instr.dir, CardDir):
-            self.pos = move(self.pos, instr.dir, instr.amt)
-        elif isinstance(instr.dir, TurnDir):
-            rotate = {
-                TurnDir.L: lambda pos: Coord(pos.x, -pos.y),
-                TurnDir.R: lambda pos: Coord(-pos.x, pos.y),
-            }
-            amt = instr.amt
-            assert amt % 90 == 0 and amt >= 0
-            while amt > 0:
-                self.pos = rotate[instr.dir](self.pos)
-                amt -= 90
-        else:
-            raise NotImplementedError(instr)
+def rotate(vector: Coord, way: str, amt: int) -> Coord:
+    '''Rotate vector left/right by the given amount.'''
+    rotator = {
+        'L': lambda vec: Coord(vec.x, -vec.y),
+        'R': lambda vec: Coord(-vec.x, vec.y),
+    }
+    assert amt % 90 == 0 and amt >= 0
+    for _ in range(amt // 90):
+        vector = rotator[way](vector)
+    return vector
 
 
 def mhdist(pos: Coord) -> int:
@@ -128,17 +64,49 @@ with open('12.input') as f:
 
 
 # part 1
-ship = Ship()
-for instr in instructions:
-    ship.move(instr)
+@dataclass
+class Ship1:
+    pos: Coord
+    face: str
+
+    def move(self, instr):
+        dir, amt = instr
+        if dir in 'NSEW':
+            self.pos = move(self.pos, dir, amt)
+        elif dir in 'LR':
+            self.face = turn(self.face, dir, amt)
+        elif dir == 'F':
+            self.move((self.face, amt))
+        else:
+            raise NotImplementedError(instr)
+
+    def accumulate(self, instructions):
+        for instr in instructions:
+            self.move(instr)
+
+
+ship = Ship1(Coord(0, 0), 'E')
+ship.accumulate(instructions)
 print(mhdist(ship.pos))
 
+
 # part 2
-ship = Ship()
-waypoint = Waypoint(Coord(1, 10))
-for instr in instructions:
-    if isinstance(instr.dir, (CardDir, TurnDir)):
-        waypoint.move(instr)
-    elif isinstance(instr.dir, MoveDir):
-        ship.move_by(waypoint.pos, instr.amt)
+@dataclass
+class Ship2(Ship1):
+    waypoint: Coord
+
+    def move(self, instr):
+        dir, amt = instr
+        if dir in 'NSEW':
+            self.waypoint = move(self.waypoint, dir, amt)
+        elif dir in 'LR':
+            self.waypoint = rotate(self.waypoint, dir, amt)
+        elif dir == 'F':
+            self.pos += self.waypoint * amt
+        else:
+            raise NotImplementedError(instr)
+
+
+ship = Ship2(Coord(0, 0), 'E', Coord(1, 10))
+ship.accumulate(instructions)
 print(mhdist(ship.pos))
